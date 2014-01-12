d166a32f807688a25e9ad4dcb13d8c97386a4379
ticmy <ticmy@foxmail.com>
2014-01-12 09:59:54 +0800

    add some icons

--- src/main/java/com/recruit/app/ui/main/MainActivity.java
+++ src/main/java/com/recruit/app/ui/main/MainActivity.java
@@ -1,5 +1,6 @@
 package com.recruit.app.ui.main;
 
+import android.app.AlertDialog.Builder;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -54,19 +55,27 @@ public class MainActivity extends SlidingMenuActivity {
 	//初始化菜单，需要按照想要的顺序排列
 	@Override
 	protected void initMenuItems() {
-		menuItems.add(new SlidingMenuItemBean(R.string.begin_search, R.drawable.ic_menu_sample_icon, new SearchFragment(), true));
-		menuItems.add(new SlidingMenuItemBean(R.string.search_record, R.drawable.ic_menu_sample_icon, null, true));
+		menuItems.add(new SlidingMenuItemBean(R.string.begin_search, R.drawable.ic_action_search, new SearchFragment(), true));
+		menuItems.add(new SlidingMenuItemBean(R.string.search_record, R.drawable.ic_action_record, null, true));
 		menuItems.add(new SlidingMenuItemBean(R.string.my_center, false));
-		menuItems.add(new SlidingMenuItemBean(R.string.my_resume, R.drawable.ic_menu_sample_icon, null, true));
-		menuItems.add(new SlidingMenuItemBean(R.string.my_message, R.drawable.ic_menu_sample_icon, new MessageListFragement(), true));
+		menuItems.add(new SlidingMenuItemBean(R.string.my_resume, R.drawable.ic_action_resume, null, true));
+		menuItems.add(new SlidingMenuItemBean(R.string.my_message, R.drawable.ic_action_message, new MessageListFragement(), true));
 		menuItems.add(new SlidingMenuItemBean(R.string.my_position, R.drawable.ic_menu_sample_icon, new PostPositionFragment(), true));
 		menuItems.add(new SlidingMenuItemBean(R.string.my_job, R.drawable.ic_menu_sample_icon, null, true));
 		menuItems.add(new SlidingMenuItemBean(R.string.system, false));
-		menuItems.add(new SlidingMenuItemBean(R.string.sys_setting, R.drawable.ic_menu_sample_icon, null, true));
+		menuItems.add(new SlidingMenuItemBean(R.string.sys_setting, R.drawable.ic_action_settings, null, true));
 		menuItems.add(new SlidingMenuItemBean(R.string.suggestion, R.drawable.ic_menu_sample_icon, null, true));
-		menuItems.add(new SlidingMenuItemBean(R.string.about_us, R.drawable.ic_menu_sample_icon, null, true));
+		menuItems.add(new SlidingMenuItemBean(R.string.about_us, R.drawable.ic_menu_sample_icon, null, true,new OnSlidingMenuItemSelectedListener(){
+
+			@Override
+			public void onSlidingMenuItemSelected(View view, int position) {
+				Builder builder = new Builder(MainActivity.this);
+				builder.setTitle(R.string.about_us).setMessage(R.string.about_us_message).create().show();
+			}
+			
+		}, false));
 //		menuItems.add(new SlidingMenuItemBean(R.string.check_for_update, R.drawable.ic_menu_sample_icon, null, true));
-		menuItems.add(new SlidingMenuItemBean(R.string.check_for_update, R.drawable.ic_menu_sample_icon, null, true, new OnSlidingMenuItemSelectedListener(){
+		menuItems.add(new SlidingMenuItemBean(R.string.check_for_update, R.drawable.ic_action_update, null, true, new OnSlidingMenuItemSelectedListener(){
 			public void onSlidingMenuItemSelected(View view, int position) {
 				Toast.makeText(getApplicationContext(), "The app is currently neweast", Toast.LENGTH_SHORT).show();
 			}
